9410f49016855c32067d4924eed9587e6540e0ba
Victor Assis <victor_assis@poli.ufrj.br>
2024-12-27 14:05:15 -0300

    fix: :bug: Fix requests validation

--- app/models/pet.py
+++ app/models/pet.py
@@ -3,6 +3,7 @@ from datetime import date
 from sqlmodel import Field, SQLModel, JSON, Column
 from uuid import uuid4 as uuid, UUID
 from enum import Enum
+from pydantic import BaseModel
 
 class Sex(str, Enum):
   MALE = 'male'
@@ -12,13 +13,28 @@ class Species(str, Enum):
   DOG = 'dog'
   CAT = 'cat'
 
-class Pet(SQLModel, table=True):
-  id: UUID = Field(default_factory=uuid, primary_key=True)
+class PetBase(SQLModel, table=False):
   name: str = Field(index=True)
   age: int = Field(index=True)
-  # birthdate: Optional[date] = Field(default=None, index=True)
-  # birthdate_is_estimate: bool = Field(default=True)
-  # sex: Sex = Field(index=True)
-  # species: Species = Field(index=True)
-  # pictures: List[str] = Field(sa_column=Column(JSON))
-  # adopted: bool = Field(default=False, index=True)
+  birthdate: Optional[date] = Field(default=None, index=True)
+  birthdate_is_estimate: bool = Field(default=True)
+  sex: Sex = Field(index=True)
+  species: Species = Field(index=True)
+  pictures: List[str] = Field(sa_column=Column(JSON))
+  adopted: bool = Field(default=False, index=True)
+
+  class Config:
+    orm_mode = True  # This is necessary for SQLModel's ORM capabilities.
+
+class PetUpdate(BaseModel):
+  name: Optional[str] = None
+  age: Optional[int] = None
+  birthdate: Optional[date] = None
+  birthdate_is_estimate: Optional[bool] = None
+  sex: Optional[Sex] = None
+  species: Optional[Species] = None
+  pictures: Optional[List[str]] = None
+  adopted: Optional[bool] = None
+
+class Pet(PetBase, table=True):
+  id: UUID = Field(default_factory=uuid, primary_key=True)
--- app/routers/pets.py
+++ app/routers/pets.py
@@ -2,9 +2,9 @@
 from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 from uuid import UUID
-from typing import Dict
+from typing import List, Dict
 
-from ..models.pet import Pet
+from ..models.pet import Pet, PetBase, PetUpdate
 from ..db.db import SessionDep
 
 router = APIRouter(
@@ -13,19 +13,20 @@ router = APIRouter(
 )
 
 @router.get('')
-async def get_pets(session: SessionDep):
+async def get_pets(session: SessionDep) -> List[Pet]:
   pets = session.exec(select(Pet)).all()
   return pets
 
 @router.post('', status_code=201)
-async def create_pet(pet: Pet, session: SessionDep) -> Pet:
+async def create_pet(_pet: PetBase, session: SessionDep) -> Pet:
+  pet = Pet(**_pet.model_dump())
   session.add(pet)
   session.commit()
   session.refresh(pet)
   return pet
 
 @router.get('/{id}')
-async def get_pet(id: UUID, session: SessionDep):
+async def get_pet(id: UUID, session: SessionDep) -> Pet:
   pet = session.exec(select(Pet).where(Pet.id == id)).first()
 
   if not pet:
@@ -33,8 +34,19 @@ async def get_pet(id: UUID, session: SessionDep):
   
   return pet
 
-@router.delete('/{id}')
-async def delete_pet(id: UUID, session: SessionDep):
+@router.delete('/{id}', responses={
+  404: {
+    "description": "Not Found Error",
+    "content": {
+      "application/json": {
+        "example": {
+          "detail": "Pet not found"
+        }
+      }
+    }
+  },
+})
+async def delete_pet(id: UUID, session: SessionDep) -> Pet:
   pet = session.exec(select(Pet).where(Pet.id == id)).first()
 
   if not pet:
@@ -43,19 +55,19 @@ async def delete_pet(id: UUID, session: SessionDep):
   session.delete(pet)
   session.commit()
 
-  return {"message": "Pet deleted"}
+  return pet
 
 @router.put('/{id}')
-async def update_pet(id: UUID, pet: Pet, session: SessionDep) -> Pet:
-  _pet = session.exec(select(Pet).where(Pet.id == id)).one()
+async def update_pet(id: UUID, pet_data: PetUpdate, session: SessionDep) -> Pet:
+  pet = session.exec(select(Pet).where(Pet.id == id)).one()
   
-  if not _pet:
+  if not pet:
     raise HTTPException(status_code=404, detail="Pet not found")
   
-  for key, value in pet.model_dump(exclude_unset=True).items(): # Only update fields provided in the request
-    setattr(_pet, key, value)
+  for key, value in pet_data.model_dump(exclude_unset=True).items(): # Only update fields provided in the request
+    setattr(pet, key, value)
 
   session.commit()
-  session.refresh(_pet)
+  session.refresh(pet)
 
-  return _pet
+  return pet
